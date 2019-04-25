92be1956bf8622136642b5ff4f02ca0571c62f7a
Ahmet <cyb3rpunk2099@gmail.com>
2019-04-25 18:46:58 +0300

    Create migrations and db update

--- src/LambdaForum.Data/ApplicationDbContext.cs
+++ src/LambdaForum.Data/ApplicationDbContext.cs
@@ -1,13 +1,31 @@
-﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+﻿using LambdaForum.Domain.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
 namespace LambdaForum.Data
 {
-    public class ApplicationDbContext : IdentityDbContext
+    public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
     {
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
             : base(options)
         {
         }
+
+        public ApplicationDbContext()
+        {
+        }
+
+        public DbSet<Forum> Forums { get; set; }
+        public DbSet<Post> Posts { get; set; }
+        public DbSet<PostReply> PostReplies { get; set; }
+
+#if DEBUG
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            base.OnConfiguring(optionsBuilder);
+            optionsBuilder.UseSqlServer(
+                "Server=(localdb)\\mssqllocaldb;Database=LambdaForum_Dev;Trusted_Connection=True;MultipleActiveResultSets=true");
+        }
+#endif
     }
 }
--- src/LambdaForum.Data/Migrations/20190425154100_InitialCreate.Designer.cs
+++ src/LambdaForum.Data/Migrations/20190425154100_InitialCreate.Designer.cs
@@ -4,14 +4,16 @@ using LambdaForum.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace LambdaForum.Data.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20190425154100_InitialCreate")]
+    partial class InitialCreate
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
